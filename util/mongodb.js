33418cc9b3ba92e62c8661b4867703ebd79aa7e0
Swapnil Dadamode <swapnildadamode@gmail.com>
2022-04-05 02:28:44 +0530

    print uri

--- util/mongodb.js
+++ util/mongodb.js
@@ -4,6 +4,7 @@ let uri = process.env.MONGODB_URI;
 let dbName = process.env.MONGODB_DB;
 
 export default async function connectToDatabase() {
+  console.log("uri "+uri+"  db "+dbName)
   const client = await MongoClient.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
